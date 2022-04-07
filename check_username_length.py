8702dfb94b0e5aab23d5a39096b3e048e268e795
CodeSimplifier1991 <heyyyworld1991@gmail.com>
2022-04-08 07:56:03 +0930

    Initial load.

---
+++ check_username_length.py
@@ -0,0 +1,11 @@
+# Get the client to enter their username on the console
+get_theUsername = input('Enter your username: ')
+
+# While the entered username is less than 8 or greater than 20 characters
+while len(get_theUsername) < 8 or len(get_theUsername) > 20:
+
+    # The client needs to enter the username again & a message will be shown as a hint
+    get_theUsername = input("Enter your username again: (Must be between 8 to 20 characters) " )
+
+# Once the username entered by the client the success message will be shown
+print("Username accepted")
