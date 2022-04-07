e950bddcc557b4daae176161807204950a84abcd
CodeSimplifier1991 <heyyyworld1991@gmail.com>
2022-04-08 08:02:17 +0930

    Fixed the comment.

--- check_username_length.py
+++ check_username_length.py
@@ -7,5 +7,5 @@ while len(get_theUsername) < 8 or len(get_theUsername) > 20:
     # The client needs to enter the username again & a message will be shown as a hint
     get_theUsername = input("Enter your username again: (Must be between 8 to 20 characters) " )
 
-# Once the username entered by the client the success message will be shown
+# Once the username entered by the client met the conditions the success message will be shown
 print("Username accepted")
